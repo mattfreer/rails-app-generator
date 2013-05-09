f7d391fc34ddf681e63f47261827da3b083366d6
Matt Freer <matt.freer@gmail.com>
2013-05-09 22:27:30 +0100

    Added backbone-support recipe

---
+++ recipes/backbone_support.rb
@@ -0,0 +1,15 @@
+if yes? "Do you want to use backbone-support?"
+  gem "backbone-support"
+
+  stategies << lambda do
+    puts "Beginning backbone-support install"
+    manifest = "application.js"
+    insert_into_file "app/assets/javascripts/#{ manifest }", :after => "//= require backbone\n" do
+      <<-END
+//= require backbone-support/support
+//= require backbone-support/observer
+//= require backbone-support/composite_view
+      END
+    end
+  end
+end
